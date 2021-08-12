880973e76c3b701d6e43180439302e4dc5a2e972
--replace <wathek.ghenimi1@gmail.com>
2021-08-11 17:23:08 -0700

    v 01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { button, Form, FormGroup, label, input, Navbar, Container } from 'react-bootstrap';
+import { FacebookLoginButton, GoogleLoginButton } from "react-social-login-buttons";
+import logo from './react_logo.png'
+// import { GoogleLoginButton } from "react-google-login-buttons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <div>
+    <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt="react_logo"
+          src={logo}
+          width="50"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      Simple login page with react
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+  
+    <Form className="login-form">
+      <h1><span className = " font-weight-bold text-center">Welcome!</span></h1>
+      <FormGroup>
+        <label>Email :</label><br/>
+        <input type="email" placeholder="Your email@"/><br/>
+        <label>Password :</label><br/>
+        <input type="password" placeholder="Your password"/><br/>
+      </FormGroup>
+        <button className="btn btn-lg btn-dark btn-block" >Log in</button>
+        <div className="pt-3">Or Continue with your social account</div>
+        <FacebookLoginButton/>
+        <GoogleLoginButton/>
+        <div className="text-center">
+          <a href="#">Sign up</a>
+          <span className="p-2"> | </span>
+          <a href="#">Forget password ?</a>
+        </div>
+    </Form>
+  </div>
   );
 }
 
